e3598cca93125ea5f6e2d0b04257e10c940eea62
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-08-15 23:40:01 +0700

    init hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,17 +13,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="fadeIn font-sans bg-black h-screen text-white">
-        <nav className="container mx-auto py-20 px-20 flex flex-row justify-between">
+      <main className="fadeIn font-sans h-screen text-white">
+        <nav className="container mx-auto h-1/6 px-20 flex flex-row justify-between items-center">
           <h1 className="font-bold text-xl">Ibrahimsyah Zairussalam </h1>
           <div className="space-x-16 flex flex-row content-center font-semibold">
+            <a href="#about-me">About Me</a>
             <a>Projects</a>
             <a>Contacts</a>
           </div>
         </nav>
         <section
           id="hero"
-          className="container mx-auto py-10 px-20 flex flex-row justify-between content-center"
+          className="container mx-auto px-20 h-5/6 flex flex-row justify-between items-center"
         >
           <div className="w-2/4">
             <h1 className="text-5xl font-bold">Hello There!</h1>
@@ -32,7 +33,22 @@ const Home: NextPage = () => {
               android native, and mobile multi-platform development. Love to
               exploring new technological things
             </h2>
+
+            <a href="#projects">
+              <div className="text-xl text-green-200 mt-10 font-bold">
+                Check out my works
+              </div>
+            </a>
           </div>
+        </section>
+        <section id="about-me" className="h-full container mx-auto py-10 px-20">
+          <h1 className="text-red-500 text-3xl font-bold mt-10">About Me</h1>
+          <p className="mt-10 text-xl leading-relaxed">
+            Hi! I am Ibrahimsyah Zairussalam. Currently studying as an
+            Information System Student in Faculty of Computer Science University
+            of Brawijaya. Highly interested on Software Development especially
+            on Web and Mobile based Application
+          </p>
         </section>
       </main>
     </>
